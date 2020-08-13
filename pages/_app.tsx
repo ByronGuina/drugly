c9e088849bcc26aa4295b8b80c613a1a8a03125b
Byron Guina <byron.guina@gmail.com>
2020-08-13 13:20:41 -0500

    Try just transparency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,13 +20,11 @@ const MyApp = ({ Component, pageProps }) => {
                     name="viewport"
                     content="width=device-width, initial-scale=1.0, shrink-to-fit=no viewport-fit=cover"
                 />
-                {/* <meta name="apple-mobile-web-app-status-bar-style" content="transparent" />
-                <meta name="apple-mobile-web-app-capable" content="yes" /> */}
-
+                <meta name="apple-mobile-web-app-status-bar-style" content="transparent" />
+                {/* <meta name="apple-mobile-web-app-capable" content="yes" /> */}
                 <title>{title}</title>
                 <meta name="description" content={descriptions} />
                 <meta property="og:type" content="website" />
-
                 {/* Essential for socials */}
                 <meta property="og:title" content={title} />
                 <meta property="og:description" content={descriptions} />
@@ -34,13 +32,11 @@ const MyApp = ({ Component, pageProps }) => {
                 <meta name="twitter:card" content="summary_large_image" />
                 {/* TODO: Base image off image in content -- once library is open. Prob will come from CDN */}
                 <meta property="og:image" content="/site.png" />
-
                 {/* Less essential */}
                 <meta property="og:site_name" content="drugly. | Anesthetic drug calculator" />
                 <meta name="twitter:site" content="@byron_guina" />
                 <meta name="twitter:creator" content="@byron_guina" />
                 {/* <meta name="theme-color" content="#sdlakd" /> */}
-
                 <link rel="shortcut icon" type="image/png" href="/favicon.png" />
                 <link rel="apple-touch-icon" href="/favicon.png" />
                 <link
