20a33f9ec756c441cb095713f52ceaecaf79f3af
Byron Guina <byron.guina@gmail.com>
2020-08-13 13:11:16 -0500

    Try 1.0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ const MyApp = ({ Component, pageProps }) => {
                 <meta charSet="utf-8" />
                 <meta
                     name="viewport"
-                    content="width=device-width, initial-scale=1, shrink-to-fit=no viewport-fit=cover"
+                    content="width=device-width, initial-scale=1.0, shrink-to-fit=no viewport-fit=cover"
                 />
 
                 <title>{title}</title>
