49c70f9520a06375106fc7e2a7a0286e0874e1bf
Byron Guina <byron.guina@gmail.com>
2020-08-14 20:22:42 -0500

    Remove unused exit prop

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,6 @@ const IndexPage = () => {
         <motion.main
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
             transition={{ type: 'tween', ease: 'easeOut', duration: 0.45 }}
         >
             <motion.h1 className="px-1 mb-2 text-5xl font-bold tracking-tight inline-block" layoutId="drugs">
