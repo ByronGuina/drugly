45998d1fb3b161df04aa2ec29e31d9df0a412408
Byron Guina <byron.guina@gmail.com>
2020-08-21 17:17:36 -0500

    Try without viewport-fit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ const MyApp = ({ Component, pageProps }) => {
                 <meta charSet="utf-8" />
                 <meta
                     name="viewport"
-                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, shrink-to-fit=no, viewport-fit=cover"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, shrink-to-fit=no"
                 />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
