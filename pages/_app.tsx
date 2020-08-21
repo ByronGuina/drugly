21e5bf5996b7baaecd23b4707fbcc091037190b6
Byron Guina <byron.guina@gmail.com>
2020-08-21 16:54:39 -0500

    Fix metatags for iOS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,10 +18,11 @@ const MyApp = ({ Component, pageProps }) => {
                 <meta charSet="utf-8" />
                 <meta
                     name="viewport"
-                    content="width=device-width, initial-scale=1.0, shrink-to-fit=no viewport-fit=cover"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, shrink-to-fit=no, viewport-fit=cover"
                 />
-                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
                 <title>{title}</title>
                 <meta name="description" content={descriptions} />
                 <meta property="og:type" content="website" />
